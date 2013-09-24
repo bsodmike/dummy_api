098390bf2abf700d1acb7358614c9d7088cc19ec
Michael de Silva <michael@mwdesilva.com>
2013-09-24 23:51:00 +0530

    Return JSON response and render in client

--- client.rb
+++ client.rb
@@ -1,5 +1,7 @@
 # encoding: utf-8
 require 'curb'
+require 'json'
+require 'pry'
 
 url = 'http://lvh.me:4567/api/'
 req_params = "client_id=foo&client_secret=bar"
@@ -7,3 +9,4 @@ c = Curl::Easy.new(url) do |curl|
   curl.timeout = 120       # timeout in 2 minutes
 end
 c.http_post(req_params)
+JSON.parse c.body_str
--- server.rb
+++ server.rb
@@ -1,7 +1,10 @@
 # encoding: utf-8
 require 'sinatra'
+require 'json'
 require 'pry'
 
 post '/api/' do
   puts "POST Params: #{params}"
+  content_type :json
+  { :key1 => 'value1', :key2 => 'value2' }.to_json
 end
